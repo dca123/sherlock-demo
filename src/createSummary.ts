f2b23b167f0d8b4df24ca1049bc2eae704581991
Devinda Senanayaka <3579142+dca123@users.noreply.github.com>
2023-07-14 17:16:57 +0800

    style: change cell fill color

--- src/createSummary.ts
+++ src/createSummary.ts
@@ -2,7 +2,12 @@ import { Workbook } from "exceljs";
 export function createSummary() {
   const workbook = new Workbook();
   const sheet = workbook.addWorksheet("Summary");
-  sheet.addRow(["Summary"]);
+  const row = sheet.addRow(["Summary"]);
+  row.getCell(1).fill = {
+    type: "pattern",
+    pattern: "solid",
+    fgColor: { argb: "FF00FF00" },
+  };
   sheet.addRow(["Total", 100]);
   return workbook;
 }
